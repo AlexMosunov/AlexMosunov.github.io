56583aa52b4b8f3d1e1671a5faddb769ceec59e5
AlexMosunov <alexmosunov@gmail.com>
2019-08-31 23:26:13 +0300

    Uploading my portfolio

---
+++ omnifood/Lingerie-bra/lingerie-bra/resources/js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function()  {
+    
+    $('.js--scroll-to-catalog').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-catalog').offset().top}, 1000);
+});
+    
+    
+$('.js--scroll-to-contact').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000);
+});
+    
+});
+
+
+
